4a49ccc717f6ea5fc35bf91aacb2eb6ad70054da
Kevin Bernfeld <kcbernfeld@gmail.com>
2023-04-22 13:48:29 -0400

    removing registration functionality

--- routes/usersRoutes.ts
+++ routes/usersRoutes.ts
@@ -1,28 +1,10 @@
 import express from "express";
 import passport from "passport";
-import bcrypt from "bcryptjs";
-import { db } from "../configs/db.config";
 import { checkAuth } from '../auth';
 import jwt from 'jsonwebtoken';
 
 export const registerUsers = () => {
   const app = express.Router();
-
-  //register
-  app.post("/register", async (req, res) => {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const results = await db.query(
-      `INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *`,
-      [req.body.username, hashedPassword]
-    );
-    console.log(results);
-    res.status(201).json({
-      status: "created",
-      data: {
-        users: results.rows,
-      },
-    });
-  });
 
   //log in
   app.post("/login", (req, res, next) => {
